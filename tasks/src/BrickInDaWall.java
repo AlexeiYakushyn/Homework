897d6767feec7f7c1f9f8693a20c54c7a7b034d8
degget <teodoksus@gmail.com>
2017-07-05 21:11:59 +0300

    Final version.

--- tasks/src/BrickInDaWall.java
+++ tasks/src/BrickInDaWall.java
@@ -6,34 +6,32 @@ public class BrickInDaWall {
     private double y;
     private double z;
     private double d;
-    private double xy;
-    private double xz;
-    private double yz;
+    private int count = 0; // count retroduces how many times brick'll path the hole.
 
     public BrickInDaWall(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    public String solution(double x, double y, double z, double d) {
         double xy = Math.sqrt(x * x + y * y);
         double xz = Math.sqrt(x * x + z * z);
         double yz = Math.sqrt(y * y + z * z);
-    }
 
-   /* public BrickInDaWall(double d) {
-        this.d = d;
-    }*/
-
-    public double setDiameter(double d) {
-        return this.d;
-    }
-
-    public String solution(double d) {
-        this.d = d;
-
-        if ((this.xy <= d) && (this.yz <= d) && (this.xz <= d)) {
-            return "Another brick in the wall!";
-        } else {
-            return "Wrong holl jackass)))";
+        if (xy < d) {
+            count++;
         }
+        if (xz < d) {
+            count++;
+        }
+        if (yz < d) {
+            count++;
+        }
+        if (!(xy < d) && !(xz < d) && !(yz < d)) {
+            return "Brick doesn't path the hole.";
+        }
+
+        return "Brick path hole with " + count + " edges.";
     }
 }
