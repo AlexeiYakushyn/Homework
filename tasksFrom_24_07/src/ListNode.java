e9d02ce5aff684b3e0b668c9cff9e4841a153366
AlexeiYakushyn <teodoksus@gmail.com>
2017-07-27 15:57:25 +0300

    PreCondition.

---
+++ tasksFrom_24_07/src/ListNode.java
@@ -0,0 +1,27 @@
+
+
+/**
+ * Created by alexei.yakushyn on 27.07.2017.
+ */
+
+// Найти n-ный с конца элемент для связного списка, который представлен следующей структурой:
+
+class ListNode {
+    private int value;
+    private ListNode next;
+
+    public ListNode(int value, ListNode next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public ListNode next() {
+        return this.next;
+    }
+
+    public int getNthElement(ListNode list, int nEl) {
+
+
+        return 0;
+    }
+}
