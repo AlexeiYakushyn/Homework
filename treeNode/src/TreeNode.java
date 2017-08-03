d4d8ee5ea19b8e3510e247e38a5fd2ae290ca80f
java2 <teodoksus@gmail.com>
2017-08-03 20:50:26 +0300

    TreeNode

---
+++ treeNode/src/TreeNode.java
@@ -0,0 +1,16 @@
+/**
+ * Created by java2 on 03.08.17.
+ */
+
+//
+public class TreeNode {
+
+    public TreeNode root;
+    public TreeNode left;
+    public TreeNode right;
+    int value;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+}
