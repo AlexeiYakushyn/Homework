0aec2ddeaa4385947d2b2a680b65d4f2a68c59b8
AlexeiYakushyn <teodoksus@gmail.com>
2017-08-06 22:42:36 +0300

    PreCondition.

--- treeNode/src/TreeNode.java
+++ treeNode/src/TreeNode.java
@@ -1,16 +1,49 @@
 /**
- * Created by java2 on 03.08.17.
+ * Created by alexei.yakushyn on 03.08.17.
  */
 
-//
+
 public class TreeNode {
 
     public TreeNode root;
     public TreeNode left;
     public TreeNode right;
     int value;
+    private TreeNode parent;
 
     public TreeNode(int value) {
         this.value = value;
     }
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setValue(int data) {
+        this.value = value;
+    }
+
+    public TreeNode getLeft() {
+        return this.left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return this.right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public TreeNode getParent() {
+        return this.parent;
+    }
+
+    public void setParent(TreeNode parent) {
+        this.parent = parent;
+    }
 }
+
