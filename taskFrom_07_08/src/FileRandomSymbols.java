81a4ef89aa51bf3ac853d1617b3bb17f333cedcd
DegGeT <teodoksus@gmail.com>
2017-08-10 16:55:49 +0300

    file file file whgn?

--- taskFrom_07_08/src/FileRandomSymbols.java
+++ taskFrom_07_08/src/FileRandomSymbols.java
@@ -1,25 +1,57 @@
+import java.io.*;
+
 /**
  * Created by alexei.yakushyn on 09.08.2017.
  */
 public class FileRandomSymbols {
 
-   /* public void CreatePassword(int length) {
-        String symbols = "qwerty";
-        StringBuilder randString = new StringBuilder();
-        int count = (int) (Math.random() * 30);
-        for (int i = 0; i < count; i++)
-            randString.append(symbols.charAt((int) (Math.random() * symbols.length())));
 
-        System.out.println(randString);
-    }*/
+    private static String testFile = "E:\\ProgrammFiles\\Homework\\Homework\\taskFrom_07_08\\src\\testFile.txt";
+    File file = new File(testFile);
+    private static String randomText;
 
-    public static void main(String[] args) {
+
+    public String  randomGen() {
         String symbols = "qwerty";
         StringBuilder randString = new StringBuilder();
         int count = (int) (Math.random() * 2000000);
         for (int i = 0; i < count; i++)
             randString.append(symbols.charAt((int) (Math.random() * symbols.length())));
+       return randomText = String.valueOf(randString);
+    }
 
-        System.out.println(randString);
+
+    public void writeInFile(String testFile, String randomText) throws IOException {
+
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            PrintWriter out = new PrintWriter(file.getAbsoluteFile());
+
+            try {
+                out.print(randomText);
+
+            } finally {
+                out.close();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(testFile)));
+//        String test = reader.readLine();
+
+        FileRandomSymbols rndsmbl = new FileRandomSymbols();
+
+       rndsmbl.writeInFile(testFile, randomText);
+        System.out.println(randomText);
+
+
+
+
     }
 }
